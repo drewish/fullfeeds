d42a93a42c3f93d4eebb130f88207e155a9d02e1
andrew morton <drewish@katherinehouse.com>
2012-01-30 01:34:13 -0500

    Almost a full rewrite to add caching using redis. Switched out the jQuery for Sizzle because it seemed a bit simpler... but might switch it back.

--- feed.js
+++ feed.js
@@ -1,137 +1,144 @@
+var async = require('async'),
+    redis = require("redis"),
+    client;
+
 var feedInfo = function(feedName) {
   var wapoLink = function(article) {
     // Skip ads.
     if (article.link.indexOf("ads.pheedo.com") === -1) {
       return article.guid;
     }
-    console.log("Skipping %s", article.link);
     return false;
   };
   var feeds = {
     'plumline': {
       url: 'http://feeds.washingtonpost.com/rss/rss_plum-line',
       urlExtractor: wapoLink,
-      contentExtractor: function($) { return $('#entrytext').html(); }
+      selector: '#entrytext',
     },
     'ezraklein': {
       url: 'http://feeds.washingtonpost.com/rss/rss_ezra-klein',
       urlExtractor: wapoLink,
-      contentExtractor: function($) { return $('#article_body').html(); }
+      selector: '#article_body'
     },
   };
   return feeds[feedName];
 }
 
-var itemFetcher = function (uri, callback) {
-  var request = require('request');
-  request(
-    { 'uri': uri, 'headers': {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'} },
-    function (err, response, body) {
-      if (err) {
-        callback(err);
-      }
-      else if (!response || response.statusCode != 200) {
-        callback(new Error('Request to ' + articleUrl + ' ended with status code: ' + (typeof response !== 'undefined' ? response.statusCode : 'unknown')));
-      }
-      else {
-        callback(null, response, body);
-      }
+var itemFetcher = function (item, callback) {
+  var request = require('request'),
+      jsdom  = require('jsdom');
+  return callback(null, item);
+
+  var key = "fullfeed:article:" + item.url;
+
+  // Try to load a cached copy.
+  client.get(key, function (err, res) {
+    if (res !== null) {
+      console.log('cache hit');
+      item.body = res;
     }
-  );
+    else {
+      console.log("%s Fetching article %s", item.feedName, item.url);
+      // Have JSDOM fetch the page and parse it...
+      jsdom.env(
+        item.url,
+        function(errors, window) {
+          var Sizzle = require("jsdom/example/sizzle/sizzle").sizzleInit(window, window.document),
+              selector = feedInfo(item.feedName).selector,
+              matches = Sizzle(selector);
+          if (matches.length > 0) {
+            item.body = matches[0].innerHTML;
+          }
+          // ...and store a cached copy.
+          client.setex(key, 60 * 60 * 24, item.body, redis.print);
+          callback(errors, item);
+          // // Add in jQuery...
+          // jsdom.jQueryify(window, function() {
+          //   // ...pull out the important bits...
+          //   item.body = feedInfo(item.feedName).contentExtractor(window.$);
+          //   // ...and store a cached copy.
+          //   client.set(key, item.body, redis.print);
+          //   callback(err, item);
+          // });
+        }
+      );
+    }
+  });
 }
 
 var processFeed = function(feedName, finalCallback) {
-  var FeedParser = require('feedparser')
-    , RSS = require('rss')
-    , async = require('async')
-    , request = require('request')
-    , jsdom  = require('jsdom');
+  var FeedParser = require('feedparser'),
+      RSS = require('rss');
 
   var feedUrl = feedInfo(feedName).url;
 
   async.auto({
-    fetch_source: function(callback) {
+    fetch_feed: function(callback) {
       // Fetch a feed, convert it to JSON and pass it to the callback.
-      console.log("Fetching %s", feedUrl);
+      console.log("%s: Fetching (%s)", feedName, feedUrl);
       (new FeedParser()).parseUrl(feedUrl, function(err, meta, articles) {
         callback(err, {meta: meta, articles: articles});
       });
     },
-    // Take the feed fetch its articles and assemble their contents into a new
-    // feed.
-    build_feed: ['fetch_source', function(callback, results) {
-
+    extract_urls: ['fetch_feed', function(callback, results) {
       // Build an array of the articles that have truthy URLs.
-      var urlExtractor = feedInfo(feedName).urlExtractor
-        , meta = results.fetch_source.meta
-        , articles = results.fetch_source.articles.filter(urlExtractor);
+      var urlExtractor = feedInfo(feedName).urlExtractor,
+          items = [];
 
-      console.log("%s [%s] (%d articles to fetch)", meta.title, meta.link, articles.length);
+      results.fetch_feed.articles.forEach(function (article) {
+        var url = urlExtractor(article);
+        if (url) {
+          items.push({feedName: feedName, url: url, article: article});
+        }
+      });
+      console.log("%s: Found %d items", feedName, items.length);
+      callback(null, items);
+    }],
+    fetch_items: ['extract_urls', function(callback, results) {
+      async.map(results.extract_urls, itemFetcher, callback);
+//      callback(null);
+    }],
+    build_feed: ['fetch_items', function(callback, results) {
+      console.log("%s: Building feed", feedName);
+      // Build an array of the articles that have truthy URLs.
+      var meta = results.fetch_feed.meta,
+          feedOut = new RSS({
+            // author: 'Dylan Greene',
+            title: meta.title,
+            description: meta.description,
+            site_url: meta.link,
+            feed_url: feedUrl,
+            // image_url: 'http://example.com/icon.png',
+          });
 
-      var feedOut = new RSS({
-        // author: 'Dylan Greene',
-        title: meta.title,
-        description: meta.description,
-        site_url: meta.link,
-        feed_url: feedUrl,
-        // image_url: 'http://example.com/icon.png',
+      results.fetch_items.forEach(function (item) {
+        if (item.body) {
+          feedOut.item({
+            title: item.article.title,
+            url: item.article.link,
+            guid: item.article.guid,
+            date: item.article.pubDate,
+            description: item.body
+          });
+        }
       });
 
-      async.forEachSeries(
-        articles,
-        function (article, finished) {
-          var articleUrl = urlExtractor(article);
-          itemFetcher(
-            articleUrl,
-            function (err, response, body) {
-              if (err) {
-                finished(err);
-              }
-              else {
-                console.log('fetched');
-                var window = jsdom.jsdom().createWindow();
-                body = body.replace(/<(\/?)script/g, '<$1nobreakage');
-                jsdom.jQueryify(window, __dirname + '/public/javascripts/jquery-1.6.1.min.js', function(win, $) {
-                  $('head').append($(body).find('head').html());
-                  $('body').append($(body).find('body').html());
-
-                  // extractor can skip an item by returning false.
-                  var content = feedInfo(feedName).contentExtractor($);
-                  if (content !== false) {
-                    feedOut.item({
-                      title: article.title,
-                      url: article.link,
-                      guid: article.guid,
-                      date: article.pubDate,
-                      description: content
-                    });
-                  }
-                  finished();
-                });
-              }
-            }
-          );
-        },
-        function (err) { callback(err, feedOut); }
-      );
+      callback(null, feedOut);
     }],
     save_feed: ['build_feed', function(callback, results) {
       var path = __dirname + '/public/' + feedName + '.xml';
       var fs = require('fs');
+      console.log("%s: Saving to %s", feedName, path);
       fs.writeFile(path, results.build_feed.xml(), function(err) {
-        if (err) {
-          console.log(err);
-          callback(err);
-        }
-        else {
-          console.log("The file was saved to %s", path);
-          callback();
-        }
+        callback(err, path);
       });
-    }]
-  });
+   }]
+  }, finalCallback);
 }
 
-
-processFeed('plumline');
-processFeed('ezraklein');
+client = redis.createClient();
+async.parallel([
+  function(callback){ processFeed('plumline', callback); },
+  function(callback){ processFeed('ezraklein', callback); }
+], function(){ client.end(); });
