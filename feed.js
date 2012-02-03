fb3801703406367f521f9fed79dc974cea965c47
andrew morton <drewish@katherinehouse.com>
2012-02-02 23:40:12 -0500

    Flipped the feed configuration around so it's more cleanly injected. Also added Paul Grahm's feed.

--- feed.js
+++ feed.js
@@ -1,108 +1,71 @@
 var async = require('async'),
     redis = require("redis"),
-    client;
+    request = require('request'),
+    jsdom  = require('jsdom'),
+    FeedParser = require('feedparser'),
+    RSS = require('rss'),
+    client = redis.createClient();
 
-var feedInfo = function(feedName) {
-  var wapoLink = function(article) {
-    // Skip ads.
-    if (article.link.indexOf("ads.pheedo.com") === -1) {
-      return article.guid;
-    }
-    return false;
-  };
-  var feeds = {
-    'plumline': {
-      url: 'http://feeds.washingtonpost.com/rss/rss_plum-line',
-      urlExtractor: wapoLink,
-      selector: '#entrytext',
-    },
-    'ezraklein': {
-      url: 'http://feeds.washingtonpost.com/rss/rss_ezra-klein',
-      urlExtractor: wapoLink,
-      selector: '#article_body'
-    },
-    'taibbi': {
-      url: 'http://www.rollingstone.com/siteServices/rss/taibbiBlog',
-      selector: '.blog-post-content'
-    }
-  };
-  return feeds[feedName];
-}
-
-var itemFetcher = function (item, callback) {
-  var request = require('request'),
-      jsdom  = require('jsdom');
-
-  var key = "fullfeed:article:" + item.url;
-
-  // Try to load a cached copy.
-  client.get(key, function (err, res) {
-    if (res !== null) {
-      item.body = res;
-      callback(err, item);
-    }
-    else {
-      console.log("%s: Fetching %s", item.feedName, item.url);
-      // Have JSDOM fetch the page and parse it...
-      jsdom.env(
-        item.url,
-        function(errors, window) {
-          var Sizzle = require("jsdom/example/sizzle/sizzle").sizzleInit(window, window.document),
-              selector = feedInfo(item.feedName).selector,
-              matches = Sizzle(selector);
-          if (matches.length > 0) {
-            item.body = matches[0].innerHTML;
-          }
-          // ...and store a cached copy.
-          client.setex(key, 60 * 60 * 24, item.body, redis.print);
-          callback(errors, item);
-          // // Add in jQuery...
-          // jsdom.jQueryify(window, function() {
-          //   // ...pull out the important bits...
-          //   item.body = feedInfo(item.feedName).contentExtractor(window.$);
-          //   // ...and store a cached copy.
-          //   client.set(key, item.body, redis.print);
-          //   callback(err, item);
-          // });
-        }
-      );
-    }
-  });
-}
-
-var processFeed = function(feedName, finalCallback) {
-  var FeedParser = require('feedparser'),
-      RSS = require('rss');
-
-  var feedUrl = feedInfo(feedName).url;
-
+var processFeed = function(config, finalCallback) {
   async.auto({
     fetch_feed: function(callback) {
       // Fetch a feed, convert it to JSON and pass it to the callback.
-      console.log("%s: Fetching (%s)", feedName, feedUrl);
-      (new FeedParser()).parseUrl(feedUrl, function(err, meta, articles) {
+      console.log("%s: Fetching (%s)", config.name, config.url);
+      (new FeedParser()).parseUrl(config.url, function(err, meta, articles) {
         callback(err, {meta: meta, articles: articles});
       });
     },
+
     extract_urls: ['fetch_feed', function(callback, results) {
       // Build an array of the articles that have truthy URLs.
-      var urlExtractor = feedInfo(feedName).urlExtractor || function(a) { return a.link; },
+      var urlExtractor = config.urlExtractor || function(a) { return a.link; },
           items = [];
 
       results.fetch_feed.articles.forEach(function (article) {
         var url = urlExtractor(article);
         if (url) {
-          items.push({feedName: feedName, url: url, article: article});
+          items.push({url: url, article: article});
         }
       });
-      console.log("%s: Found %d items", feedName, items.length);
+      console.log("%s: Found %d items", config.name, items.length);
       callback(null, items);
     }],
+
     fetch_items: ['extract_urls', function(callback, results) {
+      var itemFetcher = function (item, callback) {
+        var key = "fullfeed:article:" + item.url;
+
+        // Try to load a cached copy.
+        client.get(key, function (err, res) {
+          if (res !== null) {
+            item.body = res;
+            callback(err, item);
+            return
+          }
+          console.log("%s: Fetching %s", config.name, item.url);
+          // Have JSDOM fetch the page and parse it...
+          jsdom.env(
+            item.url,
+            function(errors, window) {
+              // jsdom has a customized version of sizzle that we can use.
+              var Sizzle = require("jsdom/example/sizzle/sizzle").sizzleInit(window, window.document),
+                  matches = Sizzle(config.selector);
+              if (matches.length > 0) {
+                item.body = matches[0].innerHTML;
+              }
+              // ...and store a cached copy for a week.
+              client.setex(key, 60 * 60 * 24 * 7, item.body, redis.print);
+              callback(errors, item);
+            }
+          );
+        });
+      }
+
       async.map(results.extract_urls, itemFetcher, callback);
     }],
+
     build_feed: ['fetch_items', function(callback, results) {
-      console.log("%s: Building feed", feedName);
+      console.log("%s: Building feed", config.name);
       // Build an array of the articles that have truthy URLs.
       var meta = results.fetch_feed.meta,
           feedOut = new RSS({
@@ -110,7 +73,7 @@ var processFeed = function(feedName, finalCallback) {
             title: meta.title,
             description: meta.description,
             site_url: meta.link,
-            feed_url: feedUrl,
+            feed_url: config.url,
             // image_url: 'http://example.com/icon.png',
           });
 
@@ -128,10 +91,11 @@ var processFeed = function(feedName, finalCallback) {
 
       callback(null, feedOut);
     }],
+
     save_feed: ['build_feed', function(callback, results) {
-      var path = __dirname + '/public/' + feedName + '.xml';
+      var path = __dirname + '/public/' + config.name + '.xml';
       var fs = require('fs');
-      console.log("%s: Saving to %s", feedName, path);
+      console.log("%s: Saving to %s", config.name, path);
       fs.writeFile(path, results.build_feed.xml(), function(err) {
         callback(err, path);
       });
@@ -139,9 +103,40 @@ var processFeed = function(feedName, finalCallback) {
   }, finalCallback);
 }
 
-client = redis.createClient();
-async.parallel([
-  function(callback){ processFeed('plumline', callback); },
-  function(callback){ processFeed('ezraklein', callback); },
-  function(callback){ processFeed('taibbi', callback); }
-], function(){ client.end(); });
+
+var wapoLink = function(article) {
+  // Skip ads.
+  return (article.link.indexOf("ads.pheedo.com") === -1) ? article.guid : false;
+};
+var feedConfig = [
+  {
+    name: 'plumline',
+    url: 'http://feeds.washingtonpost.com/rss/rss_plum-line',
+    urlExtractor: wapoLink,
+    selector: '#entrytext',
+  },
+  {
+    name: 'ezraklein',
+    url: 'http://feeds.washingtonpost.com/rss/rss_ezra-klein',
+    urlExtractor: wapoLink,
+    selector: '#article_body'
+  },
+  {
+    name: 'taibbi',
+    url: 'http://www.rollingstone.com/siteServices/rss/taibbiBlog',
+    selector: '.blog-post-content'
+  },
+  {
+    name: 'paulgraham',
+    url: 'http://www.aaronsw.com/2002/feeds/pgessays.rss',
+    selector: 'table table tr td'
+  }
+
+];
+
+var actions = [];
+feedConfig.forEach(function(config) {
+  actions.push(function (callback) { processFeed(config, callback); });
+});
+// Process the feeds in parallel and then shutdown the redis client.
+async.parallel(actions, function() { client.end(); });
